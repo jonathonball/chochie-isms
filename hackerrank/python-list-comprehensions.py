d8df81bf7d9daa24df34196f2000323f0620f3b5
Jon Ball <jonathon.ball@gmail.com>
2019-12-17 14:17:17 -0500

    Added list comprehension challenge

---
+++ hackerrank/python-list-comprehensions.py
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/list-comprehensions/problem
+# run with
+# python3 python-list-comprehensions.py < input/python-list-comprehensions.txt
+
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+
+print ([ [i, j, k] for i in range(x + 1) for j in range( y + 1) for k in range(z + 1) if i + j + k != n ])
