a7896cd67943f2fd00e75669e19f7fc6a907df97
Jon Ball <jonathon.ball@gmail.com>
2019-12-18 11:02:58 -0500

    Added alternate example for list-comprehensions.py

--- hackerrank/python/list-coprehensions.py
+++ hackerrank/python/list-coprehensions.py
@@ -8,4 +8,15 @@ if __name__ == '__main__':
     z = int(input())
     n = int(input())
 
-print ([ [i, j, k] for i in range(x + 1) for j in range( y + 1) for k in range(z + 1) if i + j + k != n ])
+print([ [i, j, k] for i in range(x + 1) for j in range( y + 1) for k in range(z + 1) if i + j + k != n ])
+
+# this is the same as this commented out code
+
+# answer = []
+# for i in range(x + 1):
+#   for j in range(y + 1):
+#     for k in range(z + 1):
+#       if i + j + k != n:
+#         answer.append([i, j, k])
+# print(answer)
+
