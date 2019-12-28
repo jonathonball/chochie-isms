cd27d19e015a51a2ceebcdb5a39202034a3d7b6c
Jon Ball <jonathon.ball@gmail.com>
2019-12-28 10:01:23 -0500

    Added finding-the-percentage

---
+++ hackerrank/python/finding-the-percentage.py
@@ -0,0 +1,18 @@
+# fields = ["name", "math", "physics", "chemistry"]
+
+# for _ in range(int(input())):
+#     raw = input().split()
+#     data = { raw[0]: { fields[i]:raw[i] for i in range(1, 4) } }
+from functools import reduce
+
+if __name__ == '__main__':
+    n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+# "{:10.4f}".format(x)
+t = "{:0.2f}".format(  reduce(lambda x,y: x+y, student_marks[query_name]) / 3 )
+print(t)
